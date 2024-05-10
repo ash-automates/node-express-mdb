140f8dc3c97fe86c06ef4b8968de72ba30efaa6d
Hachem <s.hachem@murena.io>
2024-05-10 07:44:00 +0100

    Added code for lesson 7

--- app.js
+++ app.js
@@ -3,24 +3,40 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.set("view engine", "ejs");
+
+const blogs = [
+  {
+    title: "Yoshi finds eggs",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "Mario finds stars",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "How to defeat bowser",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+];
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "about.html"));
+  res.render("about", { title: "About" });
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
-});
-
-// redirects
-app.listen(port, () => {
-  console.log(`listening on port ${port}...`);
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { title: "404" });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
 });
